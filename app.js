593bb7a9b3dcf65f13a5a2963d80cc2c35f5c2fa
William Baele <baelewilliam1999@gmail.com>
2023-03-23 16:59:38 +0100

    Added JS file

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Hi");
+
+
+document.querySelector("#urlwebhook").addEventListener("click", function() {
+
+  document.querySelector("#formwebhook").classList.remove('hidden');
+  document.querySelector("#formwebhook").classList.remove('block');
+
+});
