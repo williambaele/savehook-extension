c8c584aa275996f70c028260537a8ecd649c0ec1
William Baele <baelewilliam1999@gmail.com>
2023-03-23 17:19:57 +0100

    Added JS file

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
-console.log("Hi");
 
 
+
+
+
+
+/* WEBHOOK SAVING FUNCTION*/
+let input = document.querySelector("#formwebhook");
+let webhookform = document.querySelector("#webhookform");
+let discordwebhook = '';
+
+webhookform.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if(input.value == ''){
+    input.classList.add('outline-red-600');
+    input.value = "Please enter a value";
+  }
+  else {
+  discordwebhook = input.value;
+  input.classList.add('outline-green-600');
+  // sendWebhookTest(discordwebhook);
+  localStorage.setItem('discordwebhook', discordwebhook);
+  console.log(discordwebhook + " has been saved for futur notifications.")
+  input.value = discordwebhook;
+  }
+});
+
+
+
+
+/* SETTING BTN TOOGLE */
 document.querySelector("#urlwebhook").addEventListener("click", function() {
   var x = document.getElementById("formwebhook");
   if (x.style.display === "none") {
